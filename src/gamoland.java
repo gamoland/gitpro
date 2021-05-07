a4748d74a299177a28edebe2bbc0ebbe9ec6cd8e
gamoland <szelenyim@gmail.com>
2021-05-07 11:32:59 +0200

    add c5

--- src/gamoland.java
+++ src/gamoland.java
@@ -1,5 +1,11 @@
 public class gamoland {
-    public void cheer(String name){
-        System.out.println("Hello " + name);
+    public static void main(String[] args) {
+        String name = "Unikors";
+        String adjective = "beautiful";
+        cheer(name, adjective);
+    }
+
+    public static void cheer(String name, String adjective) {
+        System.out.println("Hello " + adjective + name);
     }
 }
